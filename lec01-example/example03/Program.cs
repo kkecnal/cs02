be63db217130194801c31aa970e64d379d70472f
Lance <Lance>
2017-06-21 13:54:23 +0800

    新增類別範例

---
+++ lec01-example/example03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student someone = new Student();
+            someone.name = "lance";
+            Console.WriteLine("Student name:{0}",
+            someone.name);
+        }
+    }
+}
